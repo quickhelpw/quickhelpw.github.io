69d97803c9416f3edcfdec2250c7a244a72d6539
TBCodec <toth.balazs.cc@gmail.com>
2020-11-19 23:45:47 +0100

    Clear calendar and getTime to console.

--- js/index.js
+++ js/index.js
@@ -44,31 +44,31 @@ function sumAll(){
     let sumAll = sumCash + Number(kmInput)*110;
     document.querySelector("span.show-AllMount").innerHTML = sumAll;
 }
-
-function createList() {
-    for (var i = 0; i < dictData.length; i++) {
-        let div = createAnyElement("div", {
-            id: k,
-            class: "smcontainer"
-        });
-        let input = createAnyElement("input", {
-            type: "radio",
-            name: "feladatok",
-            id: dictData[i][0],
-            value: dictData[i][0]
-        });
-        var label = createAnyElement("label", {
-            for: dictData[i][0],
-            name: "valazstas"
-        });
-        label.innerText = dictData[i][0];
-        document.getElementById("feladatokListája").appendChild(div);
-        document.getElementById(k).appendChild(input);
-        document.getElementById(k).appendChild(label);
-        k += 1;
-    }
-}
-
+//
+//function createList() {
+//    for (var i = 0; i < dictData.length; i++) {
+//        let div = createAnyElement("div", {
+//            id: k,
+//            class: "smcontainer"
+//        });
+//        let input = createAnyElement("input", {
+//            type: "radio",
+//            name: "feladatok",
+//            id: dictData[i][0],
+//            value: dictData[i][0]
+//        });
+//        var label = createAnyElement("label", {
+//            for: dictData[i][0],
+//            name: "valazstas"
+//        });
+//        label.innerText = dictData[i][0];
+//        document.getElementById("feladatokListája").appendChild(div);
+//        document.getElementById(k).appendChild(input);
+//        document.getElementById(k).appendChild(label);
+//        k += 1;
+//    }
+//}
+//
 function createAnyElement(name, attributes) {
     let element = document.createElement(name);
     for (let k in attributes) {
@@ -87,10 +87,10 @@ function takeMyTime() {
         let input = createAnyElement("input", {
             type: "button",
             name: "time",
-            id: "i",
+            id: i,
             value: i + ":00",
             class: "smcontainer",
-            onclick: getTime(i)
+            onclick: `getTime(${i})`
         });
         //document.getElementById("ide").appendChild(div);
         document.getElementById("ide").appendChild(input);
