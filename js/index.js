f2d8874cd53c22c3552545125f3354c26abc876a
TBCodec <toth.balazs.cc@gmail.com>
2020-11-19 23:06:36 +0100

    take my time added

--- js/index.js
+++ js/index.js
@@ -44,3 +44,66 @@ function sumAll(){
     let sumAll = sumCash + Number(kmInput)*110;
     document.querySelector("span.show-AllMount").innerHTML = sumAll;
 }
+
+function createList() {
+    for (var i = 0; i < dictData.length; i++) {
+        let div = createAnyElement("div", {
+            id: k,
+            class: "smcontainer"
+        });
+        let input = createAnyElement("input", {
+            type: "radio",
+            name: "feladatok",
+            id: dictData[i][0],
+            value: dictData[i][0]
+        });
+        var label = createAnyElement("label", {
+            for: dictData[i][0],
+            name: "valazstas"
+        });
+        label.innerText = dictData[i][0];
+        document.getElementById("feladatokListája").appendChild(div);
+        document.getElementById(k).appendChild(input);
+        document.getElementById(k).appendChild(label);
+        k += 1;
+    }
+}
+
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name);
+    for (let k in attributes) {
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
+
+function takeMyTime() {
+    i = 8;
+    while (i != 19) {
+        //let div = createAnyElement("div", {
+        //    id: i+100,
+        //    class: "smcontainer"
+        //});
+        let input = createAnyElement("input", {
+            type: "button",
+            name: "time",
+            id: "i",
+            value: i + ":00",
+            class: "smcontainer",
+            onclick: getTime(i)
+        });
+        //document.getElementById("ide").appendChild(div);
+        document.getElementById("ide").appendChild(input);
+    
+        
+        i += 1;
+    }
+
+
+}
+
+takeMyTime()
+
+function getTime(id){
+    console.log(document.getElementById(id).value);
+}
