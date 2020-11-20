6ded05de48b03893d872b228d6483839e23616df
TBCodec <toth.balazs.cc@gmail.com>
2020-11-20 23:12:44 +0100

    Véglegesnél számolja az időt, pénzt. Átadja a kezdőoldalról az órabért.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,5 @@
 window.onload = hide;
 let car = 0;
-let sumCash = 0;
-var d = new Date();
-var év = d.getFullYear()
-var hónap = d.getMonth() + 1;
-var nap = d.getDate();
-var óra = d.getHours();
-
-var getDaysInMonth = function(month,year) {
-   return new Date(year, month, 0).getDate();
-  };
-
-console.log(getDaysInMonth(hónap, év) );
-let hatralevoNapokSzama = getDaysInMonth(hónap, év) - nap;
-console.log(hatralevoNapokSzama);
 
 function hide() {
     document.getElementById("alkatOS").style.display = "none";
@@ -26,6 +12,7 @@ function os() {
     document.getElementById("alkatTS").style.display = "none";
     document.getElementById("alkatAÜ").style.display = "none";
     car = 0;
+    //passValues("os")
 }
 
 function ts() {
@@ -33,6 +20,7 @@ function ts() {
     document.getElementById("alkatOS").style.display = "none";
     document.getElementById("alkatAÜ").style.display = "none";
     car = 0;
+    //passValues("ts")
 }
 
 function aü() {
@@ -40,101 +28,9 @@ function aü() {
     document.getElementById("alkatOS").style.display = "none";
     document.getElementById("alkatTS").style.display = "none";
     car = 1;
+    //passValues("aü")
 }
 
-function sum(cb, amount) {
-    let amountInput = amount
-    //let amountInput = document.querySelector("input[name='amount-input']").value;
-    let showAmount = document.querySelector("span.show-amount");
-    if (cb.checked == true) {
-        sumCash += Number(amountInput);
-    }
-    else {
-        sumCash -= Number(amountInput);
-    }
-    showAmount.innerHTML = sumCash;
-}
-
-function sumAll() {
-    let kmInput = document.getElementById("km").value;
-    let sumAll = sumCash + Number(kmInput) * 110;
-    document.querySelector("span.show-AllMount").innerHTML = sumAll;
-}
-//
-//function createList() {
-//    for (var i = 0; i < dictData.length; i++) {
-//        let div = createAnyElement("div", {
-    //            id: k,
-//            class: "smcontainer"
-//        });
-//        let input = createAnyElement("input", {
-//            type: "radio",
-//            name: "feladatok",
-//            id: dictData[i][0],
-//            value: dictData[i][0]
-//        });
-//        var label = createAnyElement("label", {
-//            for: dictData[i][0],
-//            name: "valazstas"
-//        });
-//        label.innerText = dictData[i][0];
-//        document.getElementById("feladatokListája").appendChild(div);
-//        document.getElementById(k).appendChild(input);
-//        document.getElementById(k).appendChild(label);
-//        k += 1;
-//    }
-//}
-//
-function createAnyElement(name, attributes) {
-    let element = document.createElement(name);
-    for (let k in attributes) {
-        element.setAttribute(k, attributes[k]);
-    }
-    return element;
-}
-
-function takeMyTime(e) {
-    i = 8;
-    while (i != 19) {
-        //let div = createAnyElement("div", {
-        //    id: i+100,
-        //    class: "smcontainer"
-        //});
-        let input = createAnyElement("input", {
-            type: "button",
-            name: "time",
-            id: i,
-            value: i + ":00",
-            class: "smcontainer",
-            onclick: `getTime(${i})`,
-            style: "text-align: center"
-        });
-        //document.getElementById("ide").appendChild(div);
-        document.getElementById(hónap + e).appendChild(input);
-        i += 1;
-    }
-}
-
-function numberOfDay(){
-    for (var e = nap; e < getDaysInMonth(hónap, év)+1; e++){
-        let div = createAnyElement("div", {
-            id: hónap + e,
-            class: "container",
-            style: "text-align: left",
-            style: "background-color: lightblue"
-
-        });
-        document.getElementById("ide").appendChild(div);
-        document.getElementById(hónap + e).innerHTML = év + "." + hónap + "." + e + "      ";
-        takeMyTime(e)
-    }
-}
-
-numberOfDay()
-
-function getTime(id) {
-    console.log(document.getElementById(id).value);
-}
 
 function ellenörzés() {
     if (car == 1) {
@@ -145,3 +41,21 @@ function ellenörzés() {
     }
 }
 
+function passValues(choice) {
+    var passV = choice;
+    localStorage.setItem("choice", passV);
+    return false;
+}
+
+function sum(cb, amount) {
+    //let amountInput = amount
+    //let showAmount = document.querySelector("span.show-amount");
+    passValues(amount)
+    //if (cb.checked == true) {
+    //    sumCash += Number(amountInput);
+    //}
+    //else {
+    //    sumCash -= Number(amountInput);
+    //}
+    //showAmount.innerHTML = sumCash;
+}
---
+++ js/vegleges.js
@@ -0,0 +1,79 @@
+let óradíj = localStorage.getItem("choice");
+//console.log(óradíj);
+
+let sumCash = 0;
+var d = new Date();
+var év = d.getFullYear()
+var hónap = d.getMonth() + 1;
+var nap = d.getDate();
+var óra = d.getHours();
+
+var getDaysInMonth = function(month,year) {
+   return new Date(year, month, 0).getDate();
+  };
+
+console.log(getDaysInMonth(hónap, év) );
+let hatralevoNapokSzama = getDaysInMonth(hónap, év) - nap;
+console.log(hatralevoNapokSzama);
+
+function createAnyElement(name, attributes) {
+    let element = document.createElement(name);
+    for (let k in attributes) {
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
+
+function takeMyTime(e) {
+    i = 8;
+    while (i != 19) {
+        let input = createAnyElement("input", {
+            type: "button",
+            name: "time",
+            id: i,
+            value: i + ":00",
+            class: "smcontainer",
+            //onclick: `getTime(${i})`,
+            onclick: "pluszÓra()",
+            style: "text-align: center"
+        });
+        document.getElementById(hónap + e).appendChild(input);
+        i += 1;
+    }
+}
+
+let munkaidő = 0;
+document.querySelector("span.óradíj").innerHTML = óradíj;
+
+function pluszÓra(){
+    munkaidő += 1;
+    document.querySelector("span.munkaóra").innerHTML = munkaidő;
+}
+
+function numberOfDay(){
+    for (var e = nap; e < getDaysInMonth(hónap, év)+1; e++){
+        let div = createAnyElement("div", {
+            id: hónap + e,
+            class: "container",
+            style: "text-align: left",
+            style: "background-color: lightblue",
+
+        });
+        document.getElementById("ide").appendChild(div);
+        document.getElementById(hónap + e).innerHTML = év + "." + hónap + "." + e + "      ";
+        takeMyTime(e)
+    }
+}
+
+numberOfDay()
+
+function getTime(id) {
+    console.log(document.getElementById(id).value);
+}
+
+function sumAll() {
+    let sumAll = óradíj * munkaidő;
+    document.querySelector("span.show-AllMount").innerHTML = sumAll;
+}
+
+
